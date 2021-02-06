2c55fdac5f5222ad8ecd13685dda486be74dc729
Matt LaPaglia <mlapaglia@gmail.com>
2021-02-06 08:59:50 -0500

    migration

---
+++ OpenAlprWebhookProcessor/Data/Migrations/20210206135732_lenientplates.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenAlprWebhookProcessor.Migrations
+{
+    public partial class lenientplates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "PlateGroups",
+                newName: "PossibleNumbers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BestNumber",
+                table: "PlateGroups",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BestNumber",
+                table: "PlateGroups");
+
+            migrationBuilder.RenameColumn(
+                name: "PossibleNumbers",
+                table: "PlateGroups",
+                newName: "Number");
+        }
+    }
+}
