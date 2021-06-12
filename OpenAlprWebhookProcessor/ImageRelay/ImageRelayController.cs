80ea320bed9f61e849fcf1a8c0c463cb06974ed6
Matt LaPaglia <mlapaglia@gmail.com>
2021-06-11 22:04:08 -0400

    undo cache

--- OpenAlprWebhookProcessor/ImageRelay/ImageRelayController.cs
+++ OpenAlprWebhookProcessor/ImageRelay/ImageRelayController.cs
@@ -29,7 +29,6 @@ namespace OpenAlprWebhookProcessor.ImageRelay
             string imageId,
             CancellationToken cancellationToken)
         {
-
             return await _getImageHandler.GetImageFromAgentAsync(imageId, cancellationToken);
         }
 
