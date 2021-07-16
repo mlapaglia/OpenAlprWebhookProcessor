c55330fa46093caecad65458436dc719c8a9eccf
Matt LaPaglia <mlapaglia@gmail.com>
2021-07-16 08:58:18 -0400

    more logging around agent (#86)
    * more logging around agent
    
    * print error
    
    * even more!
    
    * package bumps
    
    * continue on error
    
    * more logging
    
    * some more logging
    
    * even more

--- OpenAlprWebhookProcessor/Alerts/Pushover/PushoverClient.cs
+++ OpenAlprWebhookProcessor/Alerts/Pushover/PushoverClient.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OpenAlprWebhookProcessor.Data;
 using System;
 using System.Net.Http;
@@ -12,6 +13,8 @@ namespace OpenAlprWebhookProcessor.Alerts.Pushover
     public class PushoverClient : IAlertClient
     {
         private const string PushOverApiUrl = "https://api.pushover.net/1/messages.json";
+
+        private const string VerifyCredentialsUrl = "https://api.pushover.net/1/users/validate.json?token={0}&user={1}";
 
         private readonly HttpClient _httpClient;
 
@@ -30,6 +33,10 @@ namespace OpenAlprWebhookProcessor.Alerts.Pushover
         {
             using (var scope = _serviceProvider.CreateScope())
             {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<PushoverClient>>();
+
+                logger.LogInformation("Sending Alert via Pushover.");
+
                 var processorContext = scope.ServiceProvider.GetRequiredService<ProcessorContext>();
 
                 var clientSettings = await processorContext.PushoverAlertClients.FirstOrDefaultAsync(cancellationToken);
@@ -58,13 +65,54 @@ namespace OpenAlprWebhookProcessor.Alerts.Pushover
 
                         if (!response.IsSuccessStatusCode)
                         {
+                            var result = await response.Content.ReadAsStringAsync(cancellationToken);
+
+                            logger.LogError("Failed to send alert via Pushover: " + result);
                             throw new InvalidOperationException("failed");
                         }
+
+                        logger.LogInformation("Alert sent via Pushover.");
                     }
                     catch (Exception ex)
                     {
+                        logger.LogError(ex, "Failed to send alert via Pushover: " + ex.Message);
                         throw new InvalidOperationException("failed");
                     }
+                }
+            }
+        }
+
+        public async Task VerifyCredentialsAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<PushoverClient>>();
+
+                logger.LogInformation("Testing credentials via Pushover.");
+
+                var processorContext = scope.ServiceProvider.GetRequiredService<ProcessorContext>();
+
+                var clientSettings = await processorContext.PushoverAlertClients.FirstOrDefaultAsync(cancellationToken);
+
+                try
+                {
+                    var result = await _httpClient.PostAsync(VerifyCredentialsUrl
+                            .Replace("{0}", clientSettings.ApiToken)
+                            .Replace("{1}", clientSettings.UserKey),
+                        null,
+                        cancellationToken);
+
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        var message = await result.Content.ReadAsStringAsync(cancellationToken);
+                        logger.LogError("Pushover credential check failed: " + message);
+                    }
+
+                    logger.LogInformation("Pushover credentials are valid.");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Pushover credential check failed: " + ex.Message);
                 }
             }
         }
