bfc05e80b7fb4d8d33329c2ceeb0d3a35e71a213
unknown <mlapaglia@gmail.com>
2023-11-23 00:37:01 -0500

    backend tests

---
+++ Tests/EfContextCreator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using OpenAlprWebhookProcessor.Data;
+
+namespace Tests
+{
+    public class EfContextCreator
+    {
+        readonly SqliteConnection _connection;
+
+        readonly DbContextOptions<ProcessorContext> _contextOptions;
+
+        public EfContextCreator()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            _contextOptions = new DbContextOptionsBuilder<ProcessorContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            using var context = new ProcessorContext(_contextOptions);
+
+            context.Database.EnsureCreated();
+        }
+
+        public ProcessorContext CreateContext() => new(_contextOptions);
+
+        public void Dispose() => _connection.Dispose();
+    }
+}
