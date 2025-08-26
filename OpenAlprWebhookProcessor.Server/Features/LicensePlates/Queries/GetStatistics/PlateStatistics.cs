8205f7ad496f59747f9c54842e3adf7c37033664
Matt LaPaglia <mlapaglia@gmail.com>
2025-08-26 01:50:02 -0400

    optimize query

--- OpenAlprWebhookProcessor.Server/Features/LicensePlates/Queries/GetStatistics/PlateStatistics.cs
+++ OpenAlprWebhookProcessor.Server/Features/LicensePlates/Queries/GetStatistics/PlateStatistics.cs
@@ -16,8 +16,11 @@ namespace OpenAlprWebhookProcessor.Features.LicensePlates.Queries.GetStatistics
     public class PlateStatisticsAggregation
     {
         public int TotalCount { get; set; }
+
         public int Last90DaysCount { get; set; }
+
         public long MinEpoch { get; set; }
+
         public long MaxEpoch { get; set; }
     }
 }
